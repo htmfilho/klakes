e083970c6a97f1b1376ff94acbcd6c500de917a6
Hildeberto Mendonca <me@hildeberto.com>
2019-03-23 10:33:49 -0400

    Fixed the uberjar packaging

--- project.clj
+++ project.clj
@@ -3,11 +3,16 @@
   :url "klakes.github.io"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure       "1.9.0"]
-                 [ring/ring-core            "1.6.3"]
-                 [http-kit                  "2.3.0"]
-                 [javax.servlet/servlet-api "2.5"]
-                 [compojure                 "1.6.1"]]
+  :dependencies [[org.clojure/clojure       "1.9.0" ]
+                 [ring/ring-core            "1.6.3" ]
+                 [http-kit                  "2.3.0" ]
+                 [javax.servlet/servlet-api "2.5"   ]
+                 [compojure                 "1.6.1" ]
+                 [selmer                    "1.12.6"]]
+  
   :min-lein-version "2.0.0"
-  :main klakes.app
-  :uberjar-name "klakes.jar")
+  :main ^:skip-aot klakes.app
+  
+  :profiles {:uberjar {:omit-source true
+                       :aot :all
+                       :uberjar-name "klakes.jar"}})
