18fb590439811685513bfeaae7a2606d1d084a94
Hildeberto Mendonca <me@hildeberto.com>
2019-03-22 23:27:13 -0400

    Integrated http-kit server

--- src/klakes/app.clj
+++ src/klakes/app.clj
@@ -1,5 +1,14 @@
 (ns klakes.app
+  (:use     [org.httpkit.server  :only  [run-server]])
+  (:require [clojure.java.browse :refer [browse-url]])
   (:gen-class))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Klakes"})
+
 (defn -main [& [args]]
-  (println "Klakes!"))
+  (browse-url "http://localhost:8080")
+  (println "To stop Klakes, type Ctrl+C.")
+  (run-server handler {:port 8080}))
